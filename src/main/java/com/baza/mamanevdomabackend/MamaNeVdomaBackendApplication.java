0d15b5a85029890f104d342ab6386c0b4a90f862
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-08 11:10:05 +0300

    create application

---
+++ src/main/java/com/baza/mamanevdomabackend/MamaNeVdomaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.baza.mamanevdomabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamaNeVdomaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MamaNeVdomaBackendApplication.class, args);
+    }
+
+}
