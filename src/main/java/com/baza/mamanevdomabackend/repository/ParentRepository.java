bb111e437e371ac9a87aa81544033c620ff2e551
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-21 11:48:53 +0300

    add method to update field isEnabled by parent's email

--- src/main/java/com/baza/mamanevdomabackend/repository/ParentRepository.java
+++ src/main/java/com/baza/mamanevdomabackend/repository/ParentRepository.java
@@ -1,6 +1,8 @@
 package com.baza.mamanevdomabackend.repository;
 
 import com.baza.mamanevdomabackend.entity.Parent;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,8 @@ public interface ParentRepository extends CrudRepository<Parent, Long> {
     Optional<Parent> findByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    @Modifying
+    @Query("update parents p set p.isEnabled = ?1 where p.email = ?2")
+    void updateIsEnabledByParentEmail(Boolean enable, String email);
 }
