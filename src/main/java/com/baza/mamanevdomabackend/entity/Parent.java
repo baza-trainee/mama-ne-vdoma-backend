fd98a85d735055ba0eb64a222c3cad9e0b581f01
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-21 16:30:07 +0300

    make parent implement UserDetails, change username to nickname and fetch type to eager

--- src/main/java/com/baza/mamanevdomabackend/entity/Parent.java
+++ src/main/java/com/baza/mamanevdomabackend/entity/Parent.java
@@ -6,19 +6,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
-@Data
+
 @Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "parents")
-public class Parent {
+public class Parent implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
+    private String nickname;
     @Email(regexp = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@\" + \"[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$")
     @Column(unique = true)
     private String email;
@@ -26,12 +31,50 @@ public class Parent {
     private Long likes;
     private Long dislikes;
     private boolean isEnabled;
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Child> children;
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<CareSchedule> schedules;
-    @OneToMany(mappedBy = "owner")
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
     private List<ChildrenGroup> groups;
     @ManyToOne
     private Location location;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return email;
+    }
+
+    public String getParentUsername() {
+        return nickname;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
