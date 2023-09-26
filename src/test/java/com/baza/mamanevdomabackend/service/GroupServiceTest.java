69ea89b75adae503607b386b43849759193e8b27
HrisKirill <hristoforow.kirill@gmail.com>
2023-09-26 15:24:41 +0300

    changed tests for group

--- src/test/java/com/baza/mamanevdomabackend/service/GroupServiceTest.java
+++ src/test/java/com/baza/mamanevdomabackend/service/GroupServiceTest.java
@@ -1,19 +1,18 @@
 package com.baza.mamanevdomabackend.service;
 
-import com.baza.mamanevdomabackend.entity.Child;
 import com.baza.mamanevdomabackend.entity.ChildrenGroup;
 import com.baza.mamanevdomabackend.entity.Parent;
 import com.baza.mamanevdomabackend.exception.GroupNotFoundException;
 import com.baza.mamanevdomabackend.repository.GroupRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-class GroupServiceTest {
+public class GroupServiceTest {
     @Mock
     private GroupRepository groupRepository;
     @Mock
@@ -30,7 +29,7 @@ class GroupServiceTest {
     private GroupService groupService;
 
 
-    @BeforeEach
+    @Before
     public void initMocks() throws Exception {
         MockitoAnnotations.openMocks(this).close();
     }
@@ -39,50 +38,22 @@ class GroupServiceTest {
         return new ChildrenGroup(1L,
                 "description",
                 new Parent(),
-                new ArrayList<>());
+                new HashSet<>());
     }
 
     @Test
-    public void testFindGroupById() {
+     void testFindGroupById() {
         when(groupRepository.findById(getTestGroup().getId()))
                 .thenReturn(Optional.of(getTestGroup()));
         assertEquals(getTestGroup(), groupService.findGroupById(getTestGroup().getId()));
     }
 
     @Test
-    public void testFindGroupByIdNotFound() {
+     void testFindGroupByIdNotFound() {
         when(groupRepository.findById(getTestGroup().getId()))
                 .thenReturn(Optional.empty());
 
         assertThrows(GroupNotFoundException.class,
                 () -> groupService.findGroupById(getTestGroup().getId()));
-    }
-
-    @Test
-    public void testUpdateGroup() {
-        when(groupRepository.findById(getTestGroup().getId()))
-                .thenReturn(Optional.of(getTestGroup()));
-
-        getTestGroup().setDescription("update description");
-
-        when(groupRepository.save(getTestGroup()))
-                .thenReturn(getTestGroup());
-
-        assertEquals(getTestGroup(), groupService.updateGroup(getTestGroup()));
-    }
-
-    @Test
-    public void testJoinChildToGroup(){
-        when(groupRepository.findById(getTestGroup().getId()))
-                .thenReturn(Optional.of(getTestGroup()));
-
-        Child child = new Child();
-        when(childService.findChildById(1L)).thenReturn(child);
-
-        getTestGroup().getChildren().add(child);
-        when(groupService.updateGroup(getTestGroup())).thenReturn(getTestGroup());
-
-        assertEquals(getTestGroup(),groupService.joinChildToGroup(
-                getTestGroup().getId(),1L));
     }
 }
